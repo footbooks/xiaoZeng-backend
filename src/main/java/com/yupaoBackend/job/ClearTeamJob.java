a175803fc492d4e2be4bfcf50e1e16c47fca1cfd
86199 <3053662971@qq.com>
2024-05-01 17:35:48 +0800

    update 项目扩展点4：给定时任务加分布式锁，防止多机部署下定时任务重复执行

--- src/main/java/com/yupaoBackend/job/ClearTeamJob.java
+++ src/main/java/com/yupaoBackend/job/ClearTeamJob.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.yupaoBackend.entity.Team;
 import com.yupaoBackend.service.TeamService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
@@ -16,20 +18,32 @@ import java.util.stream.Collectors;
 public class ClearTeamJob {
     @Resource
     private TeamService teamService;
+    @Resource
+    private RedissonClient redissonClient;
     @Scheduled(cron = "0 0 0 * * *")
     public void doClearTeam(){
-        //获取当前系统时间
-        Date date = new Date();
-        QueryWrapper<Team> queryWrapper = new QueryWrapper<>();
-        List<Team> teamList = teamService.list(queryWrapper);
-        //获取已经过期的队伍
-        List<Team> needTeamList = teamList.stream()
-                .filter(team -> {
-                    Date expireTime = team.getExpireTime();
-                    return date.after(expireTime);
-                })
-                .collect(Collectors.toList());
-        //批量删除已经过期的队伍
-        teamService.removeBatchByIds(needTeamList);
+        RLock lock = redissonClient.getLock("yupao:ClearTeamJob:doClear:lock");
+        try{
+            //获取当前系统时间
+            Date date = new Date();
+            QueryWrapper<Team> queryWrapper = new QueryWrapper<>();
+            List<Team> teamList = teamService.list(queryWrapper);
+            //获取已经过期的队伍
+            List<Team> needTeamList = teamList.stream()
+                    .filter(team -> {
+                        Date expireTime = team.getExpireTime();
+                        return date.after(expireTime);
+                    })
+                    .collect(Collectors.toList());
+            //批量删除已经过期的队伍
+            teamService.removeBatchByIds(needTeamList);
+        }catch (Exception e){
+            log.error("doClear Error:{}",e.getMessage());
+        }finally {
+            //只要锁是当前线程加的锁，才会释放
+            if(lock.isHeldByCurrentThread()){
+                lock.unlock();
+            }
+        }
     }
 }
